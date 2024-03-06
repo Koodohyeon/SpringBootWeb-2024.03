4eb78bb9fd3ddade73fbb1e4a15a6a6acfe5fd94
Koodohyeon <koodh6523@naver.com>
2024-03-06 11:52:03 +0900

    Day 2

--- src/main/java/com/example/sb/exercise/BasicController.java
+++ src/main/java/com/example/sb/exercise/BasicController.java
@@ -1,16 +1,19 @@
 package com.example.sb.exercise;
 
 
+import org.mindrot.jbcrypt.BCrypt;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/ex")
@@ -63,6 +66,31 @@ public class BasicController {
 		log.info(member.toString());		// 나중에 추가한것 syso랑 같은 의미 입니다./회사에서 syso 쓰면 대가리 깨짐 console에 info로 찍힘 debug는 너무 많아서 찾기 힘듦
 		model.addAttribute("name", member.getName());
 		return "exercise/params";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "exercise/login";
+	}
+	
+	@PostMapping("/login")
+	public String loginProc(String uid, String pwd, HttpSession session , Model model) {
+		String hashedPwd = "$2a$10$.6.xVRgbNEzWwiF/jlphgu7AZfNQnTkBItdXP75f1xX4xlfBW/e/O";
+		if (uid.equals("james") && BCrypt.checkpw(pwd, hashedPwd)) {
+			model.addAttribute("msg", uid + "님이 로그인했습니다.");
+			session.setAttribute("sessUid", uid);
+			session.setAttribute("sessUname", "제임스");
+			return "exercise/loginResult";
+		} else {
+			model.addAttribute("msg", "uid , 비밀번호를 확인하세요.");
+			return "exercise/loginResult";
+		}
+	}
+	
+	@GetMapping("/path/{uid}/{bid}")
+	@ResponseBody
+	public String path(@PathVariable String uid, @PathVariable int bid) {
+		return "<h1>uid=" + uid + ", bid=" + bid + "</h1>";
 	}
 	
 }
